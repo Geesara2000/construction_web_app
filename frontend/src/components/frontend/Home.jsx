b43f7c9e083a98a876efafa0d317cd5c1e8ffbc0
Geesara2000 <geesaraimal12@gmail.com>
2024-11-09 15:40:20 +0530

    create hero section

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import AboutImg from '../../assets/images/about-us.jpg';
 
 const Home = () => {
   return (
@@ -27,7 +28,30 @@ const Home = () => {
         </div>
       </header>
 
-    
+      <main>
+        {/* Hero section */}
+          <section className='section-1'>
+              <div className="hero d-flex align-items-center">
+                <div className="container-fluid">
+                  <div className="text-center">
+                    <span>Welcome Amazing Constructions</span>
+                    <h1>Crafting dreams with <br /> precision & excellence</h1>
+                    <p>We excel at transforming visions into reality through outstanding craftsmanship and precise <br /> attention to detail. With years of experience and a dedication to quality.</p>
+                    <div className='mt-4'>
+                      <a className='btn btn-primary'>Contact Now</a>
+                      <a className='btn btn-secondary ms-2'>View projects</a>
+                    </div>                    
+                  </div>
+                </div>
+              </div>
+          </section>
+
+         
+      </main>
+
+      <footer>
+          
+      </footer>
     </>
   )
 }
