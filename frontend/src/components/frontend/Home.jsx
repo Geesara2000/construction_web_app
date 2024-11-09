f196334a58e06dbc30f7f493d77e243ccbcc51d7
Geesara2000 <geesaraimal12@gmail.com>
2024-11-09 15:53:05 +0530

    create about section

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -46,7 +46,23 @@ const Home = () => {
               </div>
           </section>
 
-         
+           {/* about us section */}
+           <section className='section-2 py-5'>
+              <div className='container py-5'>
+                  <div className="row">
+                    <div className="col-md-6">
+                        <img src={AboutImg} className='w-100'/>
+                    </div>
+                    <div className="col-md-6">
+                        <span>About us</span>
+                        <h2>Crafting structures that last a lifetime</h2>
+                        <p>Building enduring structures requires a comprehensive approach that combines advanced materials, resilient design, routine maintenance, and sustainable practices. By drawing on historical insights and utilizing modern technology.</p>
+
+                        <p>Designing structures that stand the test of time involves a seamless blend of cutting-edge materials, durable design, ongoing upkeep, and eco-friendly practices. By combining lessons from the past with the power of modern technology.</p>
+                    </div>
+                  </div>
+              </div>
+          </section>
       </main>
 
       <footer>
