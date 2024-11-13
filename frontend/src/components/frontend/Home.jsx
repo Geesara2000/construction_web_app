e07b655e2403d1091607462ac45dbc6d53f436b1
Geesara2000 <geesaraimal12@gmail.com>
2024-11-14 00:37:42 +0530

    add about page

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import AboutImg from "../../assets/images/about-us.jpg";
 import ServiceImg from "../../assets/images/construction1.jpg";
 import ConstructionImg from "../../assets/images/construction2.jpg";
 import Header from "../common/Header";
@@ -13,6 +12,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import AvatarImg from "../../assets/images/author-2.jpg";
 import BlogImg from "../../assets/images/construction3.jpg";
+import About from "../common/About";
 
 const Home = () => {
   return (
@@ -44,32 +44,7 @@ const Home = () => {
         </section>
 
         {/* about us section */}
-        <section className="section-2 py-5">
-          <div className="container py-5">
-            <div className="row">
-              <div className="col-md-6">
-                <img src={AboutImg} className="w-100" />
-              </div>
-              <div className="col-md-6">
-                <span>About us</span>
-                <h2>Crafting structures that last a lifetime</h2>
-                <p>
-                  Building enduring structures requires a comprehensive approach
-                  that combines advanced materials, resilient design, routine
-                  maintenance, and sustainable practices. By drawing on
-                  historical insights and utilizing modern technology.
-                </p>
-
-                <p>
-                  Designing structures that stand the test of time involves a
-                  seamless blend of cutting-edge materials, durable design,
-                  ongoing upkeep, and eco-friendly practices. By combining
-                  lessons from the past with the power of modern technology.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
+        <About/>
 
         {/* our services */}
         <section className="section-3 bg-light py-5">
@@ -352,6 +327,7 @@ const Home = () => {
           </div>
         </section>
 
+        {/* testimonials */}
         <section className="section-5 py-5">
           <div className="container">
             <div className="section-header text-center">
@@ -377,7 +353,7 @@ const Home = () => {
                         width="20"
                         height="20"
                         fill="currentColor"
-                        class="bi bi-star-fill"
+                        className="bi bi-star-fill"
                         viewBox="0 0 16 16"
                       >
                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
@@ -387,7 +363,7 @@ const Home = () => {
                         width="20"
                         height="20"
                         fill="currentColor"
-                        class="bi bi-star-fill"
+                        className="bi bi-star-fill"
                         viewBox="0 0 16 16"
                       >
                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
@@ -397,63 +373,7 @@ const Home = () => {
                         width="20"
                         height="20"
                         fill="currentColor"
-                        class="bi bi-star-fill"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                      </svg>
-                    </div>
-                    <div className="content pt-4 pb-2">
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Distinctio, rerum. Neque maiores dolore eius iusto eum!
-                        Molestias, excepturi, repudiandae ut esse expedita porro
-                        iusto eligendi adipisci, molestiae tempora sed libero?
-                      </p>
-                    </div>
-                    <hr />
-                    <div className="d-flex meta">
-                      <div>
-                        <img src={AvatarImg} alt="" width={50} />
-                      </div>
-                      <div className="ps-3">
-                        <div className="name">Jone Doe</div>
-                        <div>CEO</div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="card shadow border-0">
-                  <div className="card-body p-5">
-                    <div className="rating">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        fill="currentColor"
-                        class="bi bi-star-fill"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                      </svg>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        fill="currentColor"
-                        class="bi bi-star-fill"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                      </svg>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        fill="currentColor"
-                        class="bi bi-star-fill"
+                        className="bi bi-star-fill"
                         viewBox="0 0 16 16"
                       >
                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
@@ -489,7 +409,7 @@ const Home = () => {
                         width="20"
                         height="20"
                         fill="currentColor"
-                        class="bi bi-star-fill"
+                        className="bi bi-star-fill"
                         viewBox="0 0 16 16"
                       >
                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
@@ -499,7 +419,7 @@ const Home = () => {
                         width="20"
                         height="20"
                         fill="currentColor"
-                        class="bi bi-star-fill"
+                        className="bi bi-star-fill"
                         viewBox="0 0 16 16"
                       >
                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
@@ -509,7 +429,7 @@ const Home = () => {
                         width="20"
                         height="20"
                         fill="currentColor"
-                        class="bi bi-star-fill"
+                        className="bi bi-star-fill"
                         viewBox="0 0 16 16"
                       >
                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
@@ -545,7 +465,7 @@ const Home = () => {
                         width="20"
                         height="20"
                         fill="currentColor"
-                        class="bi bi-star-fill"
+                        className="bi bi-star-fill"
                         viewBox="0 0 16 16"
                       >
                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
@@ -555,7 +475,7 @@ const Home = () => {
                         width="20"
                         height="20"
                         fill="currentColor"
-                        class="bi bi-star-fill"
+                        className="bi bi-star-fill"
                         viewBox="0 0 16 16"
                       >
                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
@@ -565,7 +485,63 @@ const Home = () => {
                         width="20"
                         height="20"
                         fill="currentColor"
-                        class="bi bi-star-fill"
+                        className="bi bi-star-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg>
+                    </div>
+                    <div className="content pt-4 pb-2">
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Distinctio, rerum. Neque maiores dolore eius iusto eum!
+                        Molestias, excepturi, repudiandae ut esse expedita porro
+                        iusto eligendi adipisci, molestiae tempora sed libero?
+                      </p>
+                    </div>
+                    <hr />
+                    <div className="d-flex meta">
+                      <div>
+                        <img src={AvatarImg} alt="" width={50} />
+                      </div>
+                      <div className="ps-3">
+                        <div className="name">Jone Doe</div>
+                        <div>CEO</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="card shadow border-0">
+                  <div className="card-body p-5">
+                    <div className="rating">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        className="bi bi-star-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        className="bi bi-star-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        className="bi bi-star-fill"
                         viewBox="0 0 16 16"
                       >
                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
@@ -595,59 +571,75 @@ const Home = () => {
             </Swiper>
           </div>
         </section>
-      </main>
-        <section className="section-6 bg-light py-5">
-          <div className="container">
-            <div className="section-header text-center">
-                <span>blog & news</span>
-                <h2>Articles & blog posts</h2>
-                <p>
-                We specialize in a wide range of construction services, including residential, commercial, and industrial projects.
-                </p>
-              </div>
-              <div className="row pt-3">
-                <div className="col-md-4">
-                  <div className="card shadow border-0">
-                    <div className="card-img-top">
-                        <img src={BlogImg} alt="" className="w-100" />
-                    </div>
-                    <div className="card-body p-4">
-                      <div className="mb-3">
-                          <a href="#" className="title">Dummy blog title</a>
-                      </div>                 
-                          <a href="#" className="btn btn-primary small">Read More</a>                     
-                    </div>
-                  </div>                 
-                </div>
-                <div className="col-md-4">
-                  <div className="card shadow border-0">
-                    <div className="card-img-top">
-                        <img src={BlogImg} alt="" className="w-100" />
-                    </div>
-                    <div className="card-body p-4">
-                      <div className="mb-3">
-                          <a href="#" className="title">Dummy blog title</a>
-                      </div>                 
-                          <a href="#" className="btn btn-primary small">Read More</a>                     
-                    </div>
-                  </div>                 
-                </div>
-                <div className="col-md-4">
-                  <div className="card shadow border-0">
-                    <div className="card-img-top">
-                        <img src={BlogImg} alt="" className="w-100" />
-                    </div>
-                    <div className="card-body p-4">
-                      <div className="mb-3">
-                          <a href="#" className="title">Dummy blog title</a>
-                      </div>                 
-                          <a href="#" className="btn btn-primary small">Read More</a>                     
-                    </div>
-                  </div>                 
-                </div>
-              </div>
+
+        {/* blog & news */}
+      <section className="section-6 bg-light py-5">
+        <div className="container">
+          <div className="section-header text-center">
+            <span>blog & news</span>
+            <h2>Articles & blog posts</h2>
+            <p>
+              We specialize in a wide range of construction services, including
+              residential, commercial, and industrial projects.
+            </p>
           </div>
-        </section>
+          <div className="row pt-3">
+            <div className="col-md-4">
+              <div className="card shadow border-0">
+                <div className="card-img-top">
+                  <img src={BlogImg} alt="" className="w-100" />
+                </div>
+                <div className="card-body p-4">
+                  <div className="mb-3">
+                    <a href="#" className="title">
+                      Dummy blog title
+                    </a>
+                  </div>
+                  <a href="#" className="btn btn-primary small">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card shadow border-0">
+                <div className="card-img-top">
+                  <img src={BlogImg} alt="" className="w-100" />
+                </div>
+                <div className="card-body p-4">
+                  <div className="mb-3">
+                    <a href="#" className="title">
+                      Dummy blog title
+                    </a>
+                  </div>
+                  <a href="#" className="btn btn-primary small">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card shadow border-0">
+                <div className="card-img-top">
+                  <img src={BlogImg} alt="" className="w-100" />
+                </div>
+                <div className="card-body p-4">
+                  <div className="mb-3">
+                    <a href="#" className="title">
+                      Dummy blog title
+                    </a>
+                  </div>
+                  <a href="#" className="btn btn-primary small">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      </main>     
       <Footer />
     </>
   );
