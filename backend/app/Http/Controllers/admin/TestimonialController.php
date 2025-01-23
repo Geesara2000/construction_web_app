21a2bc126fa2c8c1cb0483ea2d82bd5d519dd98f
Geesara2000 <geesaraimal12@gmail.com>
2025-01-23 23:50:22 +0530

    create testimonial

---
+++ backend/app/Http/Controllers/admin/TestimonialController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\TempImage;
+use App\Models\Testimonial;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
+class TestimonialController extends Controller
+{
+    //return all testimonial
+    public function index() {}
+
+    //return all single testimonial
+    public function show() {}
+
+    //store/insert a testimonial
+    public function store(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'testimonial' => 'required',
+            'citation' => 'required|unique:projects,slug'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $testimonial = new Testimonial();
+        $testimonial->testimonial = $request->testimonial;
+        $testimonial->citation = $request->citation;
+        $testimonial->save();
+
+        if($request->imageId > 0){
+
+            $tempImage = TempImage::find($request->imageId);
+            if($tempImage != null){
+                $extArray = explode('.',$tempImage->name);
+                $ext = last($extArray);
+
+                $fileName = strtotime('now').$testimonial->id.'.'.$ext;
+
+                //create small thumbnail here
+                $sourcePath = public_path('uploads/temp/'.$tempImage->name);
+                $destPath = public_path('uploads/testimonials/small/'.$fileName);
+                $manager = new ImageManager(Driver::class);
+                $image = $manager->read($sourcePath);
+                $image->coverDown(300, 300);
+                $image->save($destPath);
+
+                $testimonial->image = $fileName;
+                $testimonial->save();
+
+
+            }
+        }
+
+        return response()->json([
+            'status'=>true,
+            'message'=>'Testimonial added successfully.'
+        ]);
+    }
+}
